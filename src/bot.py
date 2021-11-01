e6c2b9efe1ffb233c55cedaabb36e1d13178047e
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2021-11-01 12:56:58 -0400

    Added Prefix Cache

--- src/bot.py
+++ src/bot.py
@@ -3,20 +3,59 @@ import discord
 import os
 from dotenv import load_dotenv
 from gears.useful import load_cogs
-from discord_slash import SlashCommand
 import json
+from motor.motor_asyncio import AsyncIOMotorClient
+
+config = json.load(open("config.json"))
+
+load_dotenv()
+
+prefix = config.get("Bot").get("Prefix")
+
+
+async def get_prefix(bot, message):
+    """Gets the prefix from built cache, if a guild isn't found (Direct Messages) assumes prefix is the below"""
+    if message.guild is None:
+        return bot.prefix
+    else:
+        return bot.prefix_cache[str(message.guild.id)]
+
+intents = discord.Intents(
+    bans=True,
+    dm_messages=True,
+    dm_reactions=True,
+    dm_typing=True,
+    emojis=True,
+    guild_messages=True,
+    guild_reactions=True,
+    guild_typing=True,
+    guilds=True,
+    integrations=True,
+    invites=True,
+    members=True,
+    messages=True,
+    presences=True,
+    reactions=True,
+    typing=True,
+    voice_states=True,
+    webhooks=True
+)
 
 bot = commands.Bot(
     command_prefix="s",
+    intents=intents,
     description="The coolest bot ever",
     Intents=discord.Intents.all()
 )
 
-bot.config = json.load(open("config.json"))
+bot.config = config
+print("Loaded Bot Config")
+bot.prefix = prefix
 
-print(bot.config)
+mongo_uri = config.get("Mongo_URL").replace("<Username>", config.get("Mongo_User")).replace("<Password>", os.getenv("Mongo_Pass"))
 
-slash = SlashCommand(bot)
+bot.mongo = AsyncIOMotorClient(mongo_uri)
+print("Loaded Bot DB")
 
 load_cogs(bot, os.listdir("src/cogs"))
 
@@ -25,5 +64,14 @@ async def on_ready():
     """On ready tell us"""
     print(f"Bot {bot.user} logged in.")
 
-load_dotenv()
-bot.run(os.getenv("BotToken"))
+@bot.event
+async def on_cache_prefixes():
+    """Cache Prefixes"""
+    prefix_db = bot.mongo["Prefixes"]
+
+    for server in prefix_db:
+        pass    
+
+bot.dispatch("cache_prefixes")
+
+bot.run(os.getenv("Bot_Token"))
