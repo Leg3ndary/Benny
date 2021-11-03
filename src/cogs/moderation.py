605deb485a4805b290a6f8efca6723748c38ed82
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2021-11-02 21:50:46 -0400

    Reformatted moderation.py

--- src/cogs/moderation.py
+++ src/cogs/moderation.py
@@ -1,30 +1,28 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands.cooldowns import BucketType 
-from discord_slash.utils.manage_components import create_button, create_actionrow, ComponentContext, wait_for_component
-from discord_slash.model import ButtonStyle
 import datetime
 from gears.style import c_get_color, c_get_emoji
-import asyncio
+
 
 class Moderation(commands.Cog):
     """Moderation related commands"""
+
     def __init__(self, bot):
         self.bot = bot
+        self.db = bot.mongo["Moderation"]
+        self.db_modlogs = self.db["Modlogs"]
 
-    @commands.command(
-        name="ban"
-    )
+    @commands.command(name="ban")
     @commands.has_permissions(ban_members=True)
     @commands.cooldown(1.0, 3.0, commands.BucketType.user)
-    async def _ban(self, ctx, user: discord.Member = None, *, reason: str = None):
+    async def ban_user(self, ctx, user: discord.Member = None, *, reason: str = None):
         """Ban a member because they were being mean"""
         if user == ctx.author:
             same_user_embed = discord.Embed(
                 title=f"Error",
                 description=f"""You cannot ban yourself!""",
                 timestamp=datetime.datetime.utcnow(),
-                color=await c_get_color("red")
+                color=await c_get_color("red"),
             )
             return await ctx.send(embed=same_user_embed)
         elif not user:
@@ -32,7 +30,7 @@ class Moderation(commands.Cog):
                 title=f"Error",
                 description=f"""The user {user} was not found""",
                 timestamp=datetime.datetime.utcnow(),
-                color=await c_get_color("red")
+                color=await c_get_color("red"),
             )
             return await ctx.send(embed=none_mentioned)
         elif user.id == 889672871620780082:
@@ -40,7 +38,7 @@ class Moderation(commands.Cog):
                 title=f"Rude",
                 description=f"""After all I've done for you, you try to ban me?""",
                 timestamp=datetime.datetime.utcnow(),
-                color=await c_get_color("yellow")
+                color=await c_get_color("yellow"),
             )
             return await ctx.send(embed=ban_bot)
         else:
@@ -57,7 +55,7 @@ class Moderation(commands.Cog):
 - {reason} -
 ```""",
                     timestamp=datetime.datetime.utcnow(),
-                    color=await c_get_color("green")
+                    color=await c_get_color("green"),
                 )
                 return await ctx.send(embed=banned_embed)
 
@@ -68,9 +66,65 @@ class Moderation(commands.Cog):
 - {e}
 ```""",
                     timestamp=datetime.datetime.utcnow(),
-                    color=await c_get_color("red")
+                    color=await c_get_color("red"),
                 )
                 await ctx.send(embed=error)
+
+    @commands.group(name="modlogs")
+    async def modlogs(self, ctx):
+        """Check someones latest modlogs"""
+        if not ctx.invoked_subcommand:
+            pass
+
+    @modlogs.command(name="channel", aliases=["set"])
+    async def modlogs_channel(self, ctx, channel: discord.TextChannel):
+        """Set a channel for modlogs"""
+
+        data = await self.db_modlogs.find_one({"_id": ctx.guild.id})
+
+        if not data:
+            document = {
+                "_id": ctx.guild.id,
+                "channel": channel.id,
+            }
+            await self.db_modlogs.insert_one(document)
+
+            modlogs_set = discord.Embed(
+                title=f"Success",
+                description=f"""Modlogs channel set to {channel.mention}""",
+                timestamp=datetime.datetime.utcnow(),
+                color=await c_get_color("green"),
+            )
+            modlogs_set.set_thumbnail(url=await c_get_emoji("image", "check"))
+            await ctx.send(embed=modlogs_set)
+
+        if data.get("channel") == channel.id:
+            same_thing = discord.Embed(
+                title=f"Error",
+                description=f"""Modlogs channel is already set to {channel.mention}""",
+                timestamp=datetime.datetime.utcnow(),
+                color=await c_get_color("yellow"),
+            )
+            await ctx.send(embed=same_thing)
+
+        else:
+            await self.db_modlogs.update_one(
+                {"_id": ctx.guild.id}, {"$set": {"channel": channel.id}}
+            )
+
+            modlogs_changed = discord.Embed(
+                title=f"Success",
+                description=f"""Modlogs channel changed to {channel.mention}""",
+                timestamp=datetime.datetime.utcnow(),
+                color=await c_get_color("green"),
+            )
+            modlogs_changed.set_thumbnail(url=await c_get_emoji("image", "check"))
+            await ctx.send(embed=modlogs_changed)
+
+    @commands.Cog.listener()
+    async def on_send_modlog(self, type, modlog):
+        """Send modlogs to the specified channel if not just return"""
+
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
