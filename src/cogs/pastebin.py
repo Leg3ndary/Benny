a660016aff284775e415730208c53a5e8310db64
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2021-11-02 21:49:37 -0400

    Reformatted pastebin.py for later use

--- src/cogs/pastebin.py
+++ src/cogs/pastebin.py
@@ -21,15 +21,12 @@ print(pastebin.create_paste(
 
 class Pastebin(commands.Cog):
     """Moderation related commands"""
+
     def __init__(self, bot):
         self.bot = bot
         bot.pastebin = Pastebin(os.getenv("Pastebin_Token"))
 
-
-    @commands.group(
-        name="pastebin",
-        aliases=["pb"]
-    )
+    @commands.group(name="pastebin", aliases=["pb"])
     async def pastebin_commands(self, ctx):
         """Loser"""
         if not ctx.invoked_subcommand:
@@ -41,13 +38,13 @@ class Pastebin(commands.Cog):
 
     @commands.Cog.listener()
     async def on_create_error_pastebin(self, name, id, error):
-        """Create a pastebin """
+        """Create a pastebin"""
         return self.bot.pastebin.create_paste(
             api_paste_code=error,
             api_paste_private=1,
             api_paste_name="Error test",
             api_paste_expire_date="N",
-            api_paste_format="python"
+            api_paste_format="python",
         )
 
 
