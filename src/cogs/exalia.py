8a6c96ba144011bed0b4f13ee912d3cde09d3a3a
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2021-11-11 20:48:11 -0500

    Updated Exalia for later use

--- src/cogs/exalia.py
+++ src/cogs/exalia.py
@@ -6,16 +6,10 @@ import asyncio
 
 
 class Exalia(commands.Cog):
-    """Moderation related commands"""
+    """Currency Game AKA Exalia"""
 
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.command()
-    async def hi(self, ctx):
-        """Loser"""
-        pass
-
 
 def setup(bot):
     bot.add_cog(Exalia(bot))
