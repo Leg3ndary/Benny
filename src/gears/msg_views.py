6119b0a65a8e9addbabd4ec9d7261215b831c397
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2021-12-12 16:18:22 -0500

    Added DeleteView to delete bots message

---
+++ src/gears/msg_views.py
@@ -0,0 +1,21 @@
+import discord
+
+class DeleteView(discord.ui.View):
+    """Delete view to delete the message from the bot"""
+    def __init__(self, slash: bool=False):
+        """ctx: The context object needed to delete the original message"""
+        self.bctx = None
+        self.slash = slash
+        super().__init__()
+
+    @discord.ui.button(
+        label="A button",
+        emoji="🗑️",
+        style=discord.ButtonStyle.danger
+    )
+    async def button_callback(self, button, interaction):
+        if not self.slash:
+            await self.bctx.delete()
+        else:
+            await self.bctx.delete_original_message()
+        await interaction.response.send_message("Message Deleted", ephemeral=True)
