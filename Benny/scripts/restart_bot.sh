3072599b6ae21b0d3dca369229e1cf3cb15f90e2
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2021-12-24 11:42:29 -0500

    Trying another thing

--- Benny/scripts/restart_bot.sh
+++ Benny/scripts/restart_bot.sh
@@ -1,4 +1,3 @@
-tmux kill-session -t Lavalink || echo Lavalink Session Not Found
-tmux kill-session -t Bennybot || echo BennyBot Session Not found
+tmux kill-server
 tmux new-session -d -s "Lavalink" /Benny/scripts/start_lavalink.sh
 tmux new-session -d -s "BennyBot" /Benny/scripts/start_bot.sh
