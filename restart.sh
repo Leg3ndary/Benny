793e0eeacf7079565b2afa30d883ff3c4799cb6d
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2021-12-27 11:49:20 -0500

    Breh

--- restart.sh
+++ restart.sh
@@ -1,9 +1,15 @@
 cd
 
 tmux kill-session -t bot
+echo Killed Bot
 tmux kill-session -t music
+echo Killed Music
+
+
+tmux new-session -d -s music "bash Benny/Benny/scripts/sl.sh"
+echo Loaded music
 
 tmux new-session -d -s bot "bash Benny/Benny/scripts/sb.sh"
-tmux new-session -d -s music "bash Benny/Benny/scripts/sl.sh"
+echo Loaded bot
 
 echo Finished
