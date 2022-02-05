c41b075a7473ee72059c6b6f017527b8a5b64931
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2022-02-04 21:10:17 -0500

    Add logging

--- Bot/bot.py
+++ Bot/bot.py
@@ -9,8 +9,16 @@ from discord.ext import commands
 from dotenv import load_dotenv
 from gears import util
 from gears.info_printer import InfoPrinter
+import logging
 
 start = time.monotonic()
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
 
 load_dotenv()
 
