0194d6fa1f901f943d3e4987e67969b32eafd493
_Leg3ndary <ben.zhou.2006@gmail.com>
2022-03-14 09:51:20 -0400

    Fix startup loop

--- Bot/bot.py
+++ Bot/bot.py
@@ -17,9 +17,9 @@ start = time.monotonic()
 
 logger = logging.getLogger("discord")
 logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+handler = logging.FileHandler(filename="logs/discord.log", encoding="utf-8", mode="w")
 handler.setFormatter(
-    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+    logging.Formatter("%(asctime)s | %(levelname)s | %(name)s | %(message)s")
 )
 logger.addHandler(handler)
 
@@ -59,85 +59,85 @@ async def get_prefix(bot, msg):
     else:
         return bot.prefixes[str(msg.guild.id)]
 
+bot = commands.Bot(
+    command_prefix=get_prefix,
+    intents=intents,
+    description="Benny Bot, a cool bot obviously",
+)
 
 async def start_bot():
     """
     Start the bot with everything it needs
     """
-    bot = commands.Bot(
-        command_prefix=get_prefix,
-        intents=intents,
-        description="Benny Bot, a cool bot obviously",
-    )
+    async with bot:
+        bot.printer = InfoPrinter(bot)
+        await bot.printer.print_load("Printer")
 
-    bot.printer = InfoPrinter(bot)
-    await bot.printer.print_load("Printer")
+        bot.config = config
+        await bot.printer.print_load("Config")
 
-    bot.config = config
-    await bot.printer.print_load("Config")
+        bot.prefix = prefix
 
-    bot.prefix = prefix
+        bot.util = util.BotUtil(bot)
+        await bot.printer.print_load("Bot Util")
 
-    bot.util = util.BotUtil(bot)
-    await bot.printer.print_load("Bot Util")
+        bot.settingsDB = await asqlite.connect("Databases/server.db")
+        await bot.printer.print_connect("Settings Database")
 
-    bot.settingsDB = await asqlite.connect("Databases/server.db")
-    await bot.printer.print_connect("Settings Database")
+        bot.musicDB = await asqlite.connect("Databases/music.db")
+        await bot.printer.print_connect("Music Database")
 
-    bot.musicDB = await asqlite.connect("Databases/music.db")
-    await bot.printer.print_connect("Music Database")
+        file_list = {}
+        total = 0
 
-    file_list = {}
-    total = 0
+        for file in await bot.util.get_files():
+            file_len = await bot.util.len_file(file)
+            file_list[file] = file_len
+            total += file_len
+        file_list["total"] = total
+        bot.file_list = file_list
 
-    for file in await bot.util.get_files():
-        file_len = await bot.util.len_file(file)
-        file_list[file] = file_len
-        total += file_len
-    file_list["total"] = total
-    bot.file_list = file_list
+        await bot.util.load_cogs(os.listdir("Bot/cogs"))
 
-    await bot.util.load_cogs(os.listdir("Bot/cogs"))
+        async def when_bot_ready():
+            """
+            On ready dispatch and print stuff
+            """
+            await bot.wait_until_ready()
+            bot.dispatch("load_musicdb")
+            bot.dispatch("load_playlists")
+            bot.dispatch("load_mongodb")
+            bot.dispatch("load_prefixes")
+            bot.dispatch("load_spotify")
+            bot.dispatch("load_profiles")
+            bot.dispatch("load_users")
+            bot.dispatch("load_redis")
+            await bot.printer.print_bot_update("LOGGED IN")
 
-    async def when_bot_ready():
-        """
-        On ready dispatch and print stuff
-        """
-        await bot.wait_until_ready()
-        bot.dispatch("load_musicdb")
-        bot.dispatch("load_playlists")
-        bot.dispatch("load_mongodb")
-        bot.dispatch("load_prefixes")
-        bot.dispatch("load_spotify")
-        bot.dispatch("load_profiles")
-        bot.dispatch("load_users")
-        bot.dispatch("load_redis")
-        await bot.printer.print_bot_update("LOGGED IN")
+        @bot.check
+        async def global_check(ctx):
+            """
+            Global check that applies to all commands
+            ├─ Check if its me, so I bypass everything
+            ├── Check if the user is blacklisted from the bot
+            ├─── Check if command is disabled
+            ├──── Check if channel/thread is being ignored
+            ├─────
+            ├──────
+            └───────
+            """
+            return True
 
-    @bot.check
-    async def global_check(ctx):
-        """
-        Global check that applies to all commands
-        ├─ Check if its me, so I bypass everything
-        ├── Check if the user is blacklisted from the bot
-        ├─── Check if command is disabled
-        ├──── Check if channel/thread is being ignored
-        ├─────
-        ├──────
-        └───────
-        """
-        return True
-
-    async with aiohttp.ClientSession() as session:
-        bot.aiosession = session
-        await bot.printer.print_connect("AIOHTTP Session")
-        end = time.monotonic()
-        await bot.printer.print_bot(
-            "",
-            f"Bot loaded in approximately {(round((end - start) * 1000, 2))/1000} seconds",
-        )
-        bot.loop.create_task(when_bot_ready())
-        await bot.start(os.getenv("Bot_Token"))
+        async with aiohttp.ClientSession() as session:
+            bot.aiosession = session
+            await bot.printer.print_connect("AIOHTTP Session")
+            end = time.monotonic()
+            await bot.printer.print_bot(
+                "",
+                f"Bot loaded in approximately {(round((end - start) * 1000, 2))/1000} seconds",
+            )
+            bot.loop.create_task(when_bot_ready())
+            await bot.start(os.getenv("Bot_Token"))
 
 
 asyncio.run(start_bot())
