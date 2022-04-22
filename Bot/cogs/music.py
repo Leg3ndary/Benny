bd717d01056f0fed56d0965d79c94b47da8a314f
_Leg3ndary <ben.zhou.2006@gmail.com>
2022-04-22 17:06:29 -0400

    Music should be fine to work without try except now

--- Bot/cogs/music.py
+++ Bot/cogs/music.py
@@ -182,12 +182,11 @@ class Music(commands.Cog):
             )
         else:
             player: wavelink.Player = ctx.voice_client
-        try:
+
             await ctx.guild.change_voice_state(
                 channel=ctx.message.author.voice.channel, self_mute=False, self_deaf=True
             )
-        except:
-            pass
+
         return player
 
     @commands.Cog.listener()
