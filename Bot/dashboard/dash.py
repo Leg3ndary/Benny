e19ce1050c8e2c76b704185a9c5db7ec630157a3
_Leg3ndary <ben.zhou.2006@gmail.com>
2022-05-02 18:42:17 -0400

    Add website

---
+++ Bot/dashboard/dash.py
@@ -0,0 +1,61 @@
+from quart import Quart
+import quart
+import quart_discord as qd
+from discord.ext import ipc
+import json
+
+
+app = Quart(__name__)
+
+config = json.load(open("config.json"))
+ipc_client = ipc.Client(secret_key=config.get("IPC").get("Secret"))
+
+app.secret_key = b"lakjhsdlkjalksjdlkajslkd"
+
+app.config["DISCORD_CLIENT_ID"] = 889672871620780082
+app.config["DISCORD_CLIENT_SECRET"] = config.get("Bot").get("Secret")
+app.config["DISCORD_REDIRECT_URI"] = "http://127.0.0.1:5000/callback"
+app.config["DISCORD_BOT_TOKEN"] = config.get("Bot").get("Token")
+
+qdiscord = qd.DiscordOAuth2Session(app)
+
+
+@app.route("/")
+async def index():
+    member_count = await ipc_client.request(
+        "get_member_count", guild_id=839605885700669441
+    )
+
+    return str(member_count)  # display member count
+
+@app.route("/login")
+async def login():
+    return await qdiscord.create_session()
+
+@app.route("/callback/")
+async def callback():
+    await qdiscord.callback()
+    return quart.redirect(quart.url_for(".me"))
+
+
+@app.errorhandler(qd.Unauthorized)
+async def redirect_unauthorized(e):
+    return quart.redirect(quart.url_for("login"))
+
+	
+@app.route("/me/")
+@qd.requires_authorization
+async def me():
+    user = await qdiscord.fetch_user()
+    return f"""
+    <html>
+        <head>
+            <title>{user.name}</title>
+        </head>
+        <body>
+            <img src='{user.avatar_url}' />
+        </body>
+    </html>"""
+
+
+app.run()
