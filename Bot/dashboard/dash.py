78a7d5450d21c47ec32554122e1f7959ea404451
_Leg3ndary <ben.zhou.2006@gmail.com>
2022-05-02 20:16:24 -0400

    Update dash

--- Bot/dashboard/dash.py
+++ Bot/dashboard/dash.py
@@ -14,7 +14,7 @@ app.secret_key = b"lakjhsdlkjalksjdlkajslkd"
 
 app.config["DISCORD_CLIENT_ID"] = 889672871620780082
 app.config["DISCORD_CLIENT_SECRET"] = config.get("Bot").get("Secret")
-app.config["DISCORD_REDIRECT_URI"] = "http://127.0.0.1:5000/callback"
+app.config["DISCORD_REDIRECT_URI"] = "http://bennybot.me/callback"
 app.config["DISCORD_BOT_TOKEN"] = config.get("Bot").get("Token")
 
 qdiscord = qd.DiscordOAuth2Session(app)
@@ -22,6 +22,11 @@ qdiscord = qd.DiscordOAuth2Session(app)
 
 @app.route("/")
 async def index():
+    """Index"""
+    return("Hello world")
+
+@app.route("/members")
+async def members():
     member_count = await ipc_client.request(
         "get_member_count", guild_id=839605885700669441
     )
