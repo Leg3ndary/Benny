abd8ce33a1a46cb858a2ce6d426f66b233955543
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2022-05-04 10:01:12 -0400

    Smart

--- startbot.sh
+++ startbot.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-tmux new-session -d -s bot -n lavalink
+tmux new-session -d -s music -n lavalink
 tmux new-session -d -s bot -n benny
+
 tmux send-keys -t music:lavalink "cd/Benny" Enter
 tmux send-keys -t music:lavalink "java -jar lavalink.jar" Enter
 
