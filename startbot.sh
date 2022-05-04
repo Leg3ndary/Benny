fd6f0a48dfdad0cb868e5a79ed5bee8fa8475b37
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2022-05-04 10:00:35 -0400

    Fix

--- startbot.sh
+++ startbot.sh
@@ -2,8 +2,8 @@
 
 tmux new-session -d -s bot -n lavalink
 tmux new-session -d -s bot -n benny
-tmux send-keys -t bot:lavalink "cd/Benny" Enter
-tmux send-keys -t bot:lavalink "java -jar lavalink.jar" Enter
+tmux send-keys -t music:lavalink "cd/Benny" Enter
+tmux send-keys -t music:lavalink "java -jar lavalink.jar" Enter
 
 tmux send-keys -t bot:benny "cd /Benny" Enter
 tmux send-keys -t bot:benny "python3 Bot/bot.py" Enter
