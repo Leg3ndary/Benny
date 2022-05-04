defcb59cb3a618ca1ed16a12162ce1233d59f7bb
ben_z_13 <57199957+teddybears123456789@users.noreply.github.com>
2022-05-04 10:18:35 -0400

    Rename and add restart.sh

---
+++ restart.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Trying to kill previous sessions."
+{
+    tmux kill-session -t bot && echo "Killed bot session"
+} || {
+    echo "No bot session found"
+}
+{
+    tmux kill-session -t lavalink && echo "Killed lavalink session"
+} || {
+    echo "No lavalink session found"
+}
+
+bash start.sh
