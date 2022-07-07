e73cb33e2eeb90061151763550f4b18e1be44aaa
_Leg3ndary <ben.zhou.2006@gmail.com>
2022-07-06 23:04:10 -0400

    Push latest errors

--- Bot/cogs/errors.py
+++ Bot/cogs/errors.py
@@ -59,17 +59,20 @@ class Errors(commands.Cog):
 
         error = getattr(error, "original", error)
 
+        traceback = None
+
         if isinstance(error, ignored_errors):
             return
 
         # Discord Errors
+
         elif isinstance(error, discord.LoginFailure):
-            traceback.print_exception(
-                type(error), error, error.__traceback__, file=sys.stderr
-            )
+            traceback = True
             log_error("Failed to login.")
+            self.bot.dispatch("log_critical", f"Failed to login.")
 
         elif isinstance(error, discord.Forbidden):
+            traceback = False
             embed = discord.Embed(
                 title=f"Error - Missing Permissions",
                 description=f"""I don't have permission to perform this action.""",
@@ -77,8 +80,10 @@ class Errors(commands.Cog):
                 color=style.Color.RED,
             )
             await ctx.send(embed=embed)
+            self.bot.dispatch("log_debug", f"[ FORBIDDEN ] HTTP request failed. HTTP Code: {error.status} Discord Code: {error.code}")
 
         elif isinstance(error, discord.NotFound):
+            traceback = False
             embed = discord.Embed(
                 title=f"Error - Not Found",
                 description=f"""I couldn't find the requested resource.""",
@@ -86,15 +91,92 @@ class Errors(commands.Cog):
                 color=style.Color.RED,
             )
             await ctx.send(embed=embed)
+            self.bot.dispatch("log_debug", f"[ NOT FOUND ] HTTP request failed. HTTP Code: {error.status} Discord Code: {error.code}")
 
-        # Catching all other http related errors
-        elif isinstance(error, discord.HTTPException):
-            traceback.print_exception(
-                type(error), error, error.__traceback__, file=sys.stderr
+        elif isinstance(error, discord.DiscordServerError):
+            traceback = False
+            embed = discord.Embed(
+                title=f"Error - Discord",
+                description=f"""Discords failing to work. Please be patient, and monitor their website [here](https://discordstatus.com/).""",
+                timestamp=discord.utils.utcnow(),
+                color=style.Color.RED,
             )
+            await ctx.send(embed=embed)
+            log_error("Encountered a 500 error from discord.")
+            self.bot.dispatch("log_critical", f"[ DISCORD 500 ] HTTP request failed. HTTP Code: {error.status} Discord Code: {error.code}")
+
+        # Catching all other HTTP related errors
+        elif isinstance(error, discord.HTTPException):
+            traceback = True
             log_error(
                 f"HTTP request failed. HTTP Code: {error.status} Discord Code: {error.code}"
             )
+            self.bot.dispatch("log_debug", f"[ HTTP FAIL ] HTTP request failed. HTTP Code: {error.status} Discord Code: {error.code}")
+
+        elif isinstance(error, discord.InvalidData):
+            traceback = True
+            embed = discord.Embed(
+                title=f"Error - Invalid Data",
+                description=f"""Discord seems to be returning invalid data, please be patient and try again.""",
+                timestamp=discord.utils.utcnow(),
+                color=style.Color.RED,
+            )
+            await ctx.send(embed=embed)
+            log_error(f"Invalid data received from discord.")
+            self.bot.dispatch("log_error", f"[ INVALID DATA ] Received invalid data from discord.")
+
+        elif isinstance(error, discord.GatewayNotFound):
+            traceback = True
+            log_error(f"Discord Gateway Not Found.")
+            self.bot.dispatch("log_critical", f"[ GATEWAY NOT FOUND ] Discord Gateway not found.")
+
+        elif isinstance(error, discord.ConnectionClosed):
+            traceback = True
+            log_error(f"Websocket Connection closed, Reason: {error.reason} (Code: {error.code} Shard: {error.shard_id if not error.shard_id else '0'}).")
+            self.bot.dispatch("log_critical", f"[ CONNECTION CLOSED ] Discord connection closed.")
+
+        elif isinstance(error, discord.PrivilegedIntentsRequired):
+            traceback = True
+            log_error(f"Missing privileged intents. (Shard {error.shard_id if not error.shard_id else '0'})")
+            self.bot.dispatch("log_critical", f"[ PRIVILEGED INTENTS ] Discord connection closed.")
+
+        elif isinstance(error, discord.InteractionResponded):
+            traceback = True
+            _type = error.interaction.type
+            interstr = "Unknown"
+            if _type == discord.InteractionType.ping:
+                interstr = "Ping"
+            elif _type == discord.InteractionType.application_command:
+                interstr = "Slash Command"
+            elif _type == discord.InteractionType.component:
+                interstr = "Component"
+            elif _type == discord.InteractionType.autocomplete:
+                interstr = "Autocomplete"
+            elif _type == discord.InteractionType.modal_submit:
+                interstr = "Modal"
+            log_error(f"[ INTERACTION RESPONDED ] Interaction type {interstr} already responded to {error.interaction.guild.name} ({error.interaction.guild.id})")
+            self.bot.dispatch("log_warn", f"[ INTERACTION RESPONDED ] Interaction type {interstr} already responded to {error.interaction.guild.name} ({error.interaction.guild.id})")
+        
+        # Majority of these will be caused by user, these are commands ext errors
+        elif isinstance(error, commands.ConversionError):
+            traceback = False
+            embed = discord.Embed(
+                title=f"Error - Conversion",
+                description=f"""I attempted to convert the provided value, but failed.""",
+                timestamp=discord.utils.utcnow(),
+                color=style.Color.RED,
+            )
+            await ctx.send(embed=embed)
+
+        elif isinstance(error, commands.MissingRequiredArgument):
+            embed = discord.Embed(
+                title="Error - Missing Argument",
+                description=f"""You seem to be missing an argument!{ctx.command}
+                Parameter: {str(error.param).split(":", maxsplit=1)[0]}""",
+                timestamp=discord.utils.utcnow(),
+                color=style.Color.RED,
+            )
+            await ctx.send(embed=embed)
 
         elif isinstance(error, commands.MemberNotFound):
             embed = discord.Embed(
@@ -104,20 +186,6 @@ class Errors(commands.Cog):
                 color=style.Color.RED,
             )
             await ctx.send(embed=embed)
-
-        elif isinstance(error, commands.ConversionError):
-            pass
-
-        elif isinstance(error, commands.MissingRequiredArgument):
-            missing_argument = discord.Embed(
-                title="Error",
-                description=f"""{ctx.command}
-                Parameter: {str(error.param).split(":", maxsplit=1)[0]}""",
-                timestamp=discord.utils.utcnow(),
-                color=style.Color.RED,
-            )
-            missing_argument.set_thumbnail(url=style.Emojis.IMAGE.cancel)
-            await ctx.send(embed=missing_argument)
 
         elif isinstance(error, commands.DisabledCommand):
             await ctx.send(f"{ctx.command} has been disabled.")
@@ -166,10 +234,11 @@ class Errors(commands.Cog):
 
         # Printing all errors out we need to know what happened, add an else when prod finally hits
         # All other Errors not returned come here. And we can just print the default TraceBack.
-        print(f"Ignoring exception in command {ctx.command}:", file=sys.stderr)
-        traceback.print_exception(
-            type(error), error, error.__traceback__, file=sys.stderr
-        )
+        if traceback or traceback is None:
+            print(f"Ignoring exception in command {ctx.command}:", file=sys.stderr)
+            traceback.print_exception(
+                type(error), error, error.__traceback__, file=sys.stderr
+            )
 
 
 async def setup(bot: commands.Bot) -> None:
