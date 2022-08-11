f5bc9e1802f286e57262302abd8f0cff8b527658
_Leg3ndary <ben.zhou.2006@gmail.com>
2022-08-11 15:11:40 -0400

    Isort and black again

--- Bot/cogs/dictionary.py
+++ Bot/cogs/dictionary.py
@@ -3,8 +3,11 @@ import datetime
 import discord
 from discord.ext import commands
 from gears import style
-from motor.motor_asyncio import (AsyncIOMotorClient, AsyncIOMotorCollection,
-                                 AsyncIOMotorDatabase)
+from motor.motor_asyncio import (
+    AsyncIOMotorClient,
+    AsyncIOMotorCollection,
+    AsyncIOMotorDatabase,
+)
 
 
 class DictDropdown(discord.ui.Select):
