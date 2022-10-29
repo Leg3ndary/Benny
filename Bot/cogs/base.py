055078fdff2efea89bcd28bd5929f5604a510cca
_Leg3ndary <ben.zhou.2006@gmail.com>
2022-10-28 23:11:42 -0400

    Remove info command

--- Bot/cogs/base.py
+++ Bot/cogs/base.py
@@ -326,61 +326,6 @@ class Base(commands.Cog):
         embed.set_image(url=user.avatar.url)
         await ctx.reply(embed=embed, view=AvatarView())
 
-    @commands.hybrid_command(
-        name="info",
-        description="""View a server members info including roles, create, and join times.""",
-        help="""View a server members info including roles, create, and join times.""",
-        brief="""Info about a member""",
-        aliases=["i"],
-        enabled=True,
-        hidden=False,
-    )
-    @commands.cooldown(2.0, 5.0, commands.BucketType.user)
-    async def info_cmd(
-        self, ctx: commands.Context, member: discord.Member = None
-    ) -> None:
-        """
-        View an member info
-        """
-        if not member:
-            member = ctx.author
-
-        embed = discord.Embed(
-            title=f"{member.name}#{member.discriminator}",
-            timestamp=discord.utils.utcnow(),
-            color=member.color,
-        )
-        embed.add_field(
-            name="Created At",
-            value=discord.utils.format_dt(member.created_at, "F"),
-            inline=False,
-        )
-        embed.add_field(
-            name="Joined At",
-            value=discord.utils.format_dt(member.joined_at, "F"),
-            inline=False,
-        )
-        embed.add_field(
-            name="Roles",
-            value=" ".join(reversed([role.mention for role in member.roles[1:43]])),
-            inline=False,
-        )
-        embed.set_author(
-            name=member.display_name,
-            icon_url=member.display_icon.url if member.display_icon else None,
-        )
-        embed.set_footer(
-            text=f"{member.id}{' - This user is a bot.' if member.bot else ''}",
-        )
-        embed.set_thumbnail(
-            url=member.display_avatar.url
-            if member.display_avatar
-            else member.avatar.url
-        )
-        await ctx.reply(embed=embed)
-
-    # Make a permissions command along with a more complicated info view for above
-
     @commands.command(
         name="charinfo",
         aliases=["ci", "char"],
